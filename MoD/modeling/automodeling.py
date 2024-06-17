98c804f6473638d95190a9c8c8d2d1e29061853d
Marco <121761685+mlinmg@users.noreply.github.com>
2024-06-17 17:35:24 +0200

    fixed model loading from hf cache

--- MoD/modeling/automodeling.py
+++ MoD/modeling/automodeling.py
@@ -1,18 +1,40 @@
 import os
 import json
+from typing import Union
 
-from .models import *  # noqa: F401
+from transformers import AutoConfig, logging, TRANSFORMERS_CACHE
+
+from .models import *  # noqa
+
+
+def check_model_config_from_hf_cache(model_name: str) -> Union[None, AutoConfig]:
+    """
+    Check if a Hugging Face model is cached locally.
+
+    :param model_name: The name of the model to check.
+    :return: True if the model is cached, False otherwise.
+    """
+    logging.set_verbosity_error()  # Suppress logging
+    try:
+        # Attempt to load the model
+        return AutoConfig.from_pretrained(model_name, local_files_only=True)
+    except OSError:
+        return None
 
 
 class AutoMoDModelForCausalLM:
     @classmethod
     def load_model_conf(cls, path):
         if not os.path.exists(os.path.join(path, 'config.json')):
-            raise ValueError(
-                f"Cannot find a config file (config.json), at the specified directory please check the path")
-        with open(os.path.join(path, 'config.json'), 'r') as f:
-            model_conf = json.load(f)
-            return model_conf['architectures'][0]
+            model_conf = check_model_config_from_hf_cache(path)
+            if not model_conf:
+                raise ValueError(
+                    "Cannot find a config file (config.json), at the specified directory please check the path")
+            return model_conf.architectures[0]
+        else:
+           with open(os.path.join(path, 'config.json'), 'r') as f:
+                model_conf = json.load(f)
+                return model_conf['architectures'][0]
 
     @classmethod
     def from_pretrained(cls, *args, **kwargs):
